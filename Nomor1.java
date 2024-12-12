14003f3cb2d410e2eecec9db479cf10ad1b5ab66
abyazzz <abyazzz1045@gmail.com>
2024-12-12 14:23:48 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 
 public class Nomor1 {
-
-    public static String inp(int N){
-        if (N % 3 == 0 && N % 5 ==0){
+    public static String inp(int n) {
+        if (n % 3 == 0 && n % 5 == 0) {
             return "FizzBuzz";
-        } else if (N  % 3 == 0){
-            return"Fizz";
-        } else if (N % 5 == 0){
+        } else if (n % 3 == 0) {
+            return "Fizz";
+        } else if (n % 5 == 0) {
             return "Buzz";
         }
-        return String.valueOf(N);
+        return String.valueOf(n);
     }
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-        System.out.println(inp(N));
+        for (int i = 0; i <= n; i++) {
+            System.out.print(inp(i) + " "); // Menggunakan `i` untuk setiap iterasi
+        }
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
